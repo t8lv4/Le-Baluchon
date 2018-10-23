479c528c3071e1fb60d8ddbbd7583a023fdaff7d
t8lv4 <morgan@t8lv4.fr>
2018-10-23 07:58:47 +0200

    Delete default VC
    Create CurrencyConverterViewControoler

---
+++ Le Baluchon/Controller/CurrencyConverterViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CurrencyConverterViewController.swift
+//  Le Baluchon
+//
+//  Created by Morgan on 23/10/2018.
+//  Copyright © 2018 Morgan. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyConverterViewController: UIViewController {
+
+    // MARK: - OUtlets
+    
+    /// Link to Convert TextField
+    @IBOutlet weak var convertTextField: UITextField!
+
+}
