6d1690cae941c13681b24d9d3d40bdd99c5fca1e
t8lv4 <morgan@t8lv4.fr>
2018-10-26 16:44:52 +0200

    Check input validity in Converter view controller

--- Le Baluchon/Controller/CurrencyConverterViewController.swift
+++ Le Baluchon/Controller/CurrencyConverterViewController.swift
@@ -10,39 +10,60 @@ import UIKit
 
 class CurrencyConverterViewController: UIViewController {
 
-    // MARK: - OUtlets
+    // MARK: - Outlets
     
     /// Link to Convert TextField
     @IBOutlet weak var convertTextField: UITextField!
 
 }
 
+// MARK: - Methods
+
+///
 extension CurrencyConverterViewController: UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         self.convertTextField.delegate = self
     }
 
     func textFieldDidBeginEditing(_ textField: UITextField) {
-        convertTextField.text = ""
+        // dim convertir label
+        // toggle terminer button
     }
 
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         convertTextField.becomeFirstResponder()
 
         guard let amount = convertTextField.text else { return false }
-        conversionRequest(for: amount)
+        checkInputValidity(input: amount)
         convertTextField.resignFirstResponder()
         return true
     }
 
 }
 
-// MARK: - request Conversion
+// MARK: - Request service
 
 extension CurrencyConverterViewController {
+
+    /**
+     Check if the user input a number (Double) to convert.
+
+     - parameter input: the input to validate
+
+     If valid, call `conversionRequest(for:)`.
+     If not, present an alert.
+     */
+    func checkInputValidity(input: String) {
+        let number = Double(input)
+        if number != nil {
+            conversionRequest(for: input)
+        } else {
+            presentVCAlert(with: "🧐", and: "Ceci n'es pas convertible en $...")
+        }
+    }
+
     /**
      Call several methods to display a converted currency.
      - get conversion rate from Fixer
@@ -50,17 +71,21 @@ extension CurrencyConverterViewController {
      - get the value to display
      */
     func conversionRequest(for amount: String) {
+        toggleActivityIndicator(shown: true)
         CurrencyConverterService.shared.query(url: Fixer.url) { (success, rate) in
+            self.toggleActivityIndicator(shown: false)
             if success, let rate = rate {
                 print("+++++++++++")
                 print(rate)
                 // call math
             } else {
-                self.presentVCAlert(with: "Les données ne sont pas diponibles")
+                self.presentVCAlert(with: "😕", and: "Les données ne sont pas diponibles.")
             }
         }
     }
 }
+
+// MARK: - Pop-up alert
 
 /// Extend the ViewController with a UIAlertController display
 extension CurrencyConverterViewController {
@@ -69,12 +94,25 @@ extension CurrencyConverterViewController {
      - A message is displayed according to the input
      - The user dismiss the alert by clicking a "OK" button
 
-     - Parameter message: The error message to be displayed
+     - Parameters:
+        - title: The alert's title
+        - message: The error message to be displayed
      */
-    func presentVCAlert(with message: String) {
-        let alertVC = UIAlertController(title: "😕", message: message, preferredStyle: .alert)
+    func presentVCAlert(with title: String, and message: String) {
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
         self.present(alertVC, animated: true, completion: nil)
+    }
+
+}
+
+// MARK: Activity Indicator
+
+extension CurrencyConverterViewController {
+
+    /// Toggle an activity indicator
+    private func toggleActivityIndicator(shown: Bool) {
+        activityIndicator.isHidden = !shown
     }
 
 }
